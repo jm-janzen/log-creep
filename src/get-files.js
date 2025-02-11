eab770f6e4b5e94716016706195dd64416038b7b
JM <jmjanzen@gmail.com>
2025-02-11 12:38:52 -0500

    fix: replace regex with substring match
    Simpler and less abuse/bug prone.
    
    While here, refactors the name 'pattern' to 'match' to be more accurate.
    Okay, so it's not a real refactor, since it changes the API, but I'm the
    only one using this so there. :P
    
    Also, I wish I could come up with a better name than 'match'.

--- src/get-files.js
+++ src/get-files.js
@@ -1,12 +1,11 @@
 import fs from 'fs'
 
 
-export function getFiles(path = '/', pattern) {
+export function getFiles(path = '/', match = '') {
     const { BASE_DIR } = process.env
     // TODO Check if dir, else throw err
     // TODO Ensure dir not escaping BASE_DIR (sneaky /../../ for example)
     return fs.readdirSync(`${BASE_DIR}/${path}`)
-        // TODO Check if RegExp safe
         // TODO Filter out dirs? Maybe just note...
-        .filter(fileName => RegExp(pattern).test(fileName))
+        .filter(fileName => fileName.contains(match))
 }
