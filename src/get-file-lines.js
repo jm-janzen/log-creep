045aab58cd77c130d92231cdf08142fff7d174f8
JM <jmjanzen@gmail.com>
2025-02-11 18:35:11 -0500

    fix: read large files line by line
    Resolves issue where we were loading everything into memory at once, but
    this reads from the top of the file down. I like how it's pretty
    semantic looking, but it does not strictly... work as the customer
    wants, lol.
    
    Committing this mostly to show one attempt.

--- src/get-file-lines.js
+++ src/get-file-lines.js
@@ -5,12 +5,38 @@ import readline from 'readline'
 export async function getFileLines(filePath, numLines = 0, match = '') {
     const { BASE_DIR } = process.env
 
-    // TODO Actually handle large files efficiently
-    return fs
-        .readFileSync(`${BASE_DIR}/${filePath}`)
-        .toString()
-        .split(/\n/)
-        .filter(line => line.includes(match)) // FIXME This is horribly, horribly inefficient
-        .slice(numLines * -1)
-        .reverse()
+    return new Promise((resolve, reject) => {
+        const lines = []
+        const stream = fs.createReadStream(`${BASE_DIR}/${filePath}`)
+        // TODO Investigate if straight up buffer is preferrable
+        const lr = readline.createInterface({ input: stream })
+
+        lr.on('error', (error) => {
+            stream.close
+
+            reject(error)
+        })
+
+        lr.on('line', (line) => {
+            // TODO Start from bottom?
+            if (lines.length < numLines) {
+                // TODO Handle case where no 'match' specified
+                // Shouldn't have to check for substring on each line!
+                if (line.includes(match)) {
+                    lines.push(line)
+                }
+            } else {
+                // TODO Check difference between this and rl.close()
+                stream.close()
+
+                resolve(lines)
+            }
+
+        })
+
+        lr.on('close', () => {
+            resolve(lines)
+        })
+    })
+
 }
