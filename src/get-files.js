148e58e36e1f3e74d7828d81cd64835c9fff2372
JM <jmjanzen@gmail.com>
2025-02-11 18:36:56 -0500

    fix: use correct method name

--- src/get-files.js
+++ src/get-files.js
@@ -7,5 +7,5 @@ export function getFiles(path = '/', match = '') {
     // TODO Ensure dir not escaping BASE_DIR (sneaky /../../ for example)
     return fs.readdirSync(`${BASE_DIR}/${path}`)
         // TODO Filter out dirs? Maybe just note...
-        .filter(fileName => fileName.contains(match))
+        .filter(fileName => fileName.includes(match))
 }
