6a49c5e5999149bf4c57c377bf7b6c06d7d608cd
JM <jmjanzen@gmail.com>
2025-02-12 10:07:31 -0500

    refactor: convenience variable

--- src/get-files.js
+++ src/get-files.js
@@ -3,9 +3,10 @@ import fs from 'fs'
 
 export function getFiles(path = '/', match = '') {
     const { BASE_DIR } = process.env
+    const fullPath = `${BASE_DIR}/${path}`
     // TODO Check if dir, else throw err
     // TODO Ensure dir not escaping BASE_DIR (sneaky /../../ for example)
-    return fs.readdirSync(`${BASE_DIR}/${path}`)
+    return fs.readdirSync(fullPath)
         // TODO Filter out dirs? Maybe just note...
         .filter(fileName => fileName.includes(match))
 }
