6769b493b2f57322de40c8229d3eee3c21513ad6
JM <jmjanzen@gmail.com>
2025-02-13 20:51:16 -0500

    fix: use fs.readSync
    Pardon the messy patch. Functionality is not finished, but
    basics work.

--- src/get-file-lines.js
+++ src/get-file-lines.js
@@ -1,33 +1,31 @@
 import fs from 'fs'
 
 
-export async function getFileLines(filePath, numLines = 0, match = '') {
+export async function getFileLines(filePath, numLines = 1, match) {
     const { BASE_DIR } = process.env
     const fullPath = `${BASE_DIR}/${filePath}`
     const size = fs.statSync(fullPath).size
-    const options = {
-        start: 0,
-        end: size,
-    }
+    const fd = fs.openSync(fullPath)
     const matchingLines = []
 
     // TODO Actually loop over file from bottom up
-    return new Promise((resolve, reject) => {
-        const stream = fs.createReadStream(fullPath, options)
+    return new Promise((resolve) => {
+        let start = size
+        while (start > 0) {
+            const length = 32
+            const buffer = Buffer.alloc(length)
 
+            start = start - length
 
-        stream.on('error', (error) => {
-            stream.close
+            fs.readSync(fd, buffer, { start, length })
 
-            reject(error)
-        })
+            const text = buffer.toString()
 
-        stream.on('data', (chunk) => {
+            console.log(`Reading from ${start} to ${start + length} out of ${size}`, {text})
 
-            const lines = chunk.toString().split('\n')
+            const lines = text.split('\n')
 
             for (const line of lines) {
-                console.log(`Checking if ${match} matching ${line}`)
                 if (!match) {
                     matchingLines.push(line)
                 } else if (line.includes(match)) {
@@ -36,15 +34,11 @@ export async function getFileLines(filePath, numLines = 0, match = '') {
 
                 if (matchingLines.length >= numLines) {
                     resolve(matchingLines)
-                    return
                 }
             }
+        }
 
-        })
-
-        stream.on('close', () => {
-            resolve(matchingLines)
-        })
+        resolve(matchingLines)
     })
 
 }
