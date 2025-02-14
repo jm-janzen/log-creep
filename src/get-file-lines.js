b7ab4175c20150851da966f7eed1633c4781e7fa
JM <jmjanzen@gmail.com>
2025-02-13 22:45:51 -0500

    doc: add jsdocs, other comments
    Some of this stuff can seem a bit esoteric if you aren't used to it.

--- src/get-file-lines.js
+++ src/get-file-lines.js
@@ -1,6 +1,13 @@
 import fs from 'fs'
 
 
+/**
+ *
+ * @param {string} filePath valid path starting with BASE_DIR
+ * @param {number} numLines number of lines to return
+ * @param {string} match text to match within lines (if any)
+ * @returns {Promise<string[]|Error>}
+ */
 export async function getFileLines(filePath, numLines, match) {
     const { BASE_DIR } = process.env
     const fullPath = `${BASE_DIR}/${filePath}`
@@ -19,10 +26,11 @@ export async function getFileLines(filePath, numLines, match) {
 
             fs.readSync(fd, buffer, { position: start, length })
 
+            // Shift incomplete line back on to our text 'buffer' (not a real buffer)
+            // on to our text 'buffer' (not a real buffer) for next read (when we have
+            // a complete line)
             text = buffer + text
             const lines = text.split('\n')
-
-            // Save incomplete line back on to our text 'buffer' (not a real buffer)
             text = lines.shift()
 
             while (lines.length) {
